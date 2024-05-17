22c855bf8f41b0562fd8ed6a5a50c1b7312cb99a
Joao Gottardo <jvgotta1@gmail.com>
2024-05-17 20:43:47 -0300

    chore: chaging to expo

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import Profile from './src/pages/profile/Pofile';
+import {SafeAreaView} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Profile />
+    </SafeAreaView>
+  );
+}
+
+export default App;
