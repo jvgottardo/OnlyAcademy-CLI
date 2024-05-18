c69d0504c5d33789eba3cd7043e2d62b7bd05f42
Joao Gottardo <jvgotta1@gmail.com>
2024-05-17 21:36:16 -0300

    feat: add camera

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-
 import React from 'react';
-
+import Camera from './src/pages/camera/Camera';
 import Profile from './src/pages/profile/Pofile';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Profile />
+    <SafeAreaView style={styles.container}>
+      <Camera/>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Isso assegura que SafeAreaView use todo o espaço disponível
+  }
+});
 
 export default App;
